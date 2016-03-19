4a5d5636220d4fc946321d336491c6aea720cc1e
Elinxer <654753115@qq.com>
2016-03-19 09:29:27 +0800

    添加完整测试模板

---
+++ WebContent/src/js/musicList.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Smohan on 2015/5/15.
+ */
+
+var musicList = [
+    {
+        title : '爱囚',
+        singer : '庄心妍',
+        cover  : 'src/img/Maroon5.jpg',
+        src    : 'musics/q111.mp3',
+		lyric  : "src/data/爱囚.lrc"
+    },
+    {
+        title : '车站',
+        singer : '李健',
+        cover  : 'src/img/Maroon5.jpg',
+        src    : 'musics/q111.mp3',
+		lyric  : "src/data/chezhan.lrc"
+    },
+    {
+        title : '年轮 ',
+        singer : '张碧晨',
+        cover  : 'src/img/Maroon5.jpg',
+        src    : 'musics/q111.mp3',
+		lyric  : "src/data/nianlun.lrc"
+    },
+    {
+        title : '她说',
+        singer : '张碧晨',
+        cover  : 'src/img/Maroon5.jpg',
+        src    : 'musics/q111.mp3',
+		lyric  : null
+    },
+    {
+        title : '赤血长殷',
+        singer : '王凯',
+        cover  : 'src/img/Maroon5.jpg',
+        src    : 'musics/q111.mp3',
+		lyric  : null
+    },
+    {
+        title : 'My Love',
+        singer : 'WestLife',
+        cover  : 'src/img/Maroon5.jpg',
+        src    : 'musics/q111.mp3',
+		lyric  : null
+    }
+];
