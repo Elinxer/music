64fbfead7e519234aac21525d37f954441fa9628
elinxer <654753115@qq.com>
2016-04-07 20:15:48 +0800

    删除用户表

--- music_user.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `music_user` VALUES (138, 'admin', 'admin', 'admin@qq.com', '1459484341', '127.0.0.1', '1459484341', '');
-INSERT INTO `music_user` VALUES (139, '', '', '', '1459484380', '127.0.0.1', '1459484380', '');
-INSERT INTO `music_user` VALUES (140, 'walle', 'walle', 'walle@qq.com', '1459485229', '127.0.0.1', '1459485229', '');
